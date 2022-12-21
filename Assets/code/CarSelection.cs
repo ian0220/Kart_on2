4bd1f3d4c7e1786c3e0d2ab5005f872ebbc9a62b
Boy <boybest441@gmail.com>
2022-12-21 15:01:21 +0100

    Lerp

--- Assets/code/CarSelection.cs
+++ Assets/code/CarSelection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarSelection : MonoBehaviour
 {
@@ -9,11 +10,20 @@ public class CarSelection : MonoBehaviour
     [SerializeField] private float moveAmmount;
     [SerializeField] private float minMoveAmmount;
     [SerializeField] private float maxMoveAmmount;
+    [SerializeField] private float lerpTime;
+    public Vector3 point;
     public GameObject currentCar;
+    private bool moveToTarget = false;
+    private float LerpOutput;
+    public Button ArrowLeft;
+    public Button ArrowRight;
+
+
 
     private void Start()
     {
         carNumber = 0;
+        point = transform.position;
     }
 
     void Update()
@@ -27,24 +37,86 @@ public class CarSelection : MonoBehaviour
         {
             transform.position = new Vector3(maxMoveAmmount, transform.position.y, transform.position.z);
         }
+
+
+        //
+        if (moveToTarget)
+        {
+            
+            transform.position = Vector3.Lerp(transform.position, point, LerpOutput);
+            LerpOutput += Time.deltaTime * lerpTime;
+            ArrowLeft.interactable = false;
+            ArrowRight.interactable = false;
+
+            if (LerpOutput >= 0.98f)
+            {
+                LerpOutput = 0f;
+                moveToTarget = false;
+                transform.position = point;
+                ArrowLeft.interactable = true;
+                ArrowRight.interactable = true;
+            }
+
+            
+        }
+
+
     }
 
     public void GoLeft()
     {
-        gameObject.transform.position = new Vector3(transform.position.x - moveAmmount, transform.position.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x - moveAmmount, transform.position.y, transform.position.z);
+
+        if (transform.position.x > minMoveAmmount)
+        {
+        point = new Vector3(transform.position.x - moveAmmount, transform.position.y, transform.position.z);
+        MoveCamera();
+        }
+        else
+        {
+            transform.position = point;
+        }
+
         if (carNumber > 0)
+        {
+            MoveCamera();
             carNumber--;
+        }
+
+        Debug.Log("left");
     }
 
     public void GoRight()
     {
-        gameObject.transform.position = new Vector3(transform.position.x + moveAmmount, transform.position.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x + moveAmmount, transform.position.y, transform.position.z);
+
+        if(transform.position.x < maxMoveAmmount)
+        {
+        point = new Vector3(transform.position.x + moveAmmount, transform.position.y, transform.position.z);
+        MoveCamera();
+        }
+
+        else
+        {
+            transform.position = point;
+        }
+
         if (carNumber < racingCars.Length - 1)
+        {
+            MoveCamera();
             carNumber++;
+        }
+        Debug.Log("right");
     }
 
     public void SelectCar()
     {
         currentCar = racingCars[carNumber];
+    }
+
+    public void MoveCamera()
+    {
+        //
+        moveToTarget = true;
     }
 }
