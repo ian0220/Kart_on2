9ebf3690f16bc73f788e08128aedacc0a9d681a1
LukeR2003 <78425989+LukeR2003@users.noreply.github.com>
2023-01-10 12:39:22 +0100

    stomme auto uptdate

--- Assets/code/NewCar/OldNewCar/SimpleCarController.cs
+++ Assets/code/NewCar/OldNewCar/SimpleCarController.cs
@@ -6,7 +6,7 @@ public class SimpleCarController : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
-    private float steerAngle;
+    private float steeringAngle;
 
     public WheelCollider frontDriverW, frontPassengerW;
     public WheelCollider rearDriverW, rearPassengerW;
@@ -15,29 +15,47 @@ public class SimpleCarController : MonoBehaviour
     public float maxSteerAngle = 30f;
     public float motorForce = 50f;
 
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+
     public void GetInput()
     {
-
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     private void Steer()
     {
-
+        steeringAngle = maxSteerAngle * horizontalInput;
+        frontDriverW.steerAngle = steeringAngle;
+        frontPassengerW.steerAngle = steeringAngle;
     }
 
     private void Accelerate()
     {
-
+        frontDriverW.motorTorque = verticalInput * motorForce;
+        frontPassengerW.motorTorque = verticalInput * motorForce;
     }
 
     private void UpdateWheelPoses()
     {
-
+        UpdateWheelPose(frontDriverW, frontDriverT);
+        UpdateWheelPose(frontPassengerW, frontPassengerT);
+        UpdateWheelPose(rearDriverW, rearDriverT);
+        UpdateWheelPose(rearPassengerW, rearPassengerT);
     }
 
     private void UpdateWheelPose(WheelCollider wheelCollider, Transform transform)
     {
+        Vector3 position = transform.position;
+        Quaternion rotation = transform.rotation;
 
+        wheelCollider.GetWorldPose(out position, out rotation);
     }
 
 
